8cf7b490f1b653b2b5ffed77cee257ee0f7ae580
inasumairah99 <inasumairah99@gmail.com>
2026-01-04 21:59:17 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+st.title("Student Pass/Fail Prediction System")
+
+st.write("Enter student academic scores to predict outcome.")
+
+math = st.slider("Math Score", 0, 100, 75)
+reading = st.slider("Reading Score", 0, 100, 75)
+writing = st.slider("Writing Score", 0, 100, 75)
+
+if st.button("Predict"):
+    avg_score = np.mean([math, reading, writing])
+    
+    if avg_score >= 90:
+        st.success("Prediction: PASS")
+    else:
+        st.error("Prediction: FAIL")
